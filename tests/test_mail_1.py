a52a15281c287e82e2c54bdb291b242103187ef7
Ярослав <Yagavdv14@yandex.ru>
2025-01-27 00:22:52 +0300

    Готово с yield

--- tests/test_mail_1.py
+++ tests/test_mail_1.py
@@ -2,13 +2,23 @@ import pytest
 
 
 @pytest.fixture()
-def before_start():
-    print('\nПсевдодействия до прогона теста')
+def enter_exit_positive():
+    print('\nПсевдодействия до прогона теста positive')
+    yield
+    print('\nПсевдодействия после прогона теста')
 
 
-def test_send_mail_positive(before_start):
+@pytest.fixture()
+def enter_exit_negative():
+    print('\nПсевдодействия до прогона теста negative')
+    yield
+    print('\nПсевдодействия после прогона теста')
+
+
+def test_send_mail_positive(enter_exit_negative):
     print('\nСообщение отправлено')
 
 
-def test_send_mail_negative(before_start):
+def test_send_mail_negative(enter_exit_positive):
     print('\nСообщение не отправлено')
+
