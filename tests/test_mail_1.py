3c2d31055f2238d20ff44a839e6cd71fdc4b50f9
Ярослав <Yagavdv14@yandex.ru>
2025-03-26 13:33:04 +0300

    переделал с учетом замечаний

--- tests/test_mail_1.py
+++ tests/test_mail_1.py
@@ -1,8 +1,9 @@
 import pytest
 
-def test_send_mail_positive(fixture_function,fixture_module,fixture_package):
+
+def test_send_mail_positive(fixture_function, fixture_module, fixture_package):
     print('\nСообщение отправлено')
 
 
-def test_send_mail_negative(fixture_function,fixture_module,fixture_package):
+def test_send_mail_negative(fixture_function, fixture_module, fixture_package):
     print('\nСообщение не отправлено')
